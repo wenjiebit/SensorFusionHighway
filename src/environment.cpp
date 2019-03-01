52275f70e1432bda658de9a9e811ae612c304e31
awbrown90 <aaronwbrown@msn.com>
2019-03-01 11:57:15 -0800

    Formatting: broke up initalization functions in environment.cpp

--- src/environment.cpp
+++ src/environment.cpp
@@ -8,80 +8,89 @@
 // using templates for processPointClouds so also include .cpp to help linker
 #include "processPointClouds.cpp"
 
+std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr& viewer)
+{
 
-// TODO:: Add an argument for a point processor object
+	std::vector<Car> cars;
+	
+	Car egoCar( Vect3(0,0,0), Vect3(4,2,2), Color(0,1,0), "egoCar");
+	cars.push_back(egoCar);
+	
+	Car car1( Vect3(15,0,0), Vect3(4,2,2), Color(0,0,1), "car1");
+	cars.push_back(car1);
+	
+	Car car2( Vect3(8,-4,0), Vect3(4,2,2), Color(0,0,1), "car2");
+	cars.push_back(car2);
+		
+	Car car3( Vect3(-12,4,0), Vect3(4,2,2), Color(0,0,1), "car3");
+	cars.push_back(car3);
+
+	if(renderScene)
+	{
+		renderHighway(viewer);
+		egoCar.render(viewer);
+		car1.render(viewer);
+		car2.render(viewer);
+		car3.render(viewer);
+	}
+
+	return cars;
+
+}
+
 void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
 {
 	// ----------------------------------------------------
-  // -----Open 3D viewer and display simple highway -----
-  // ----------------------------------------------------
+  	// -----Open 3D viewer and display simple highway -----
+  	// ----------------------------------------------------
 	
 	// RENDER OPTIONS
-  bool render_scene = true;
-  
-  std::vector<Car> cars;
+ 	bool renderScene = true;
+ 	std::vector<Car> cars = initHighway(renderScene, viewer);
 	
-  Car egoCar( Vect3(0,0,0), Vect3(4,2,2), Color(0,1,0), "egoCar");
-  cars.push_back(egoCar);
+	// TODO:: Create lidar sensor 
+
+	// TODO:: Create point processor
   
-  Car car1( Vect3(15,0,0), Vect3(4,2,2), Color(0,0,1), "car1");
-  cars.push_back(car1);
-  
-  Car car2( Vect3(8,-4,0), Vect3(4,2,2), Color(0,0,1), "car2");
-  cars.push_back(car2);
- 	
-  Car car3( Vect3(-12,4,0), Vect3(4,2,2), Color(0,0,1), "car3");
-  cars.push_back(car3);
-  
-  // render environment
-  if(render_scene)
-  {
-  	renderHighway(viewer);
-  	egoCar.render(viewer);
-  	car1.render(viewer);
-  	car2.render(viewer);
-  	car3.render(viewer);
-  }
-	
-  // TODO:: Create lidar sensor 
-  
+}
+
+//setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
+void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& viewer)
+{
+
+  	viewer->setBackgroundColor (0, 0, 0);
+  	
+  	// set camera position and angle
+  	viewer->initCameraParameters();
+  	// distance away in meters
+  	int distance = 16;
+  	
+  	switch(setAngle)
+  	{
+  		case XY : viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0); break;
+  		case TopDown : viewer->setCameraPosition(0, 0, distance, 1, 0, 1); break;
+  		case Side : viewer->setCameraPosition(0, -distance, 0, 0, 0, 1); break;
+  		case FPS : viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
+  	}
+  	if(setAngle!=FPS)
+  		viewer->addCoordinateSystem (1.0);
+
 }
 
 int main (int argc, char** argv)
 {
+	std::cout << "starting enviroment" << std::endl;
 
-  std::cout << "starting enviroment" << std::endl;
+	pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
 
-  pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-  viewer->setBackgroundColor (0, 0, 0);
+	CameraAngle setAngle = XY;
+  	initCamera(setAngle, viewer);
+
+  	simpleHighway(viewer);
   
-  // set camera position and angle
-  viewer->initCameraParameters();
-  // distance away in meters
-  int distance = 16;
-  // CAN CHANGE ARGUMENT HERE TO SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
-  CameraAngle setAngle = XY;
-  
-  switch(setAngle)
-  {
-  	case XY : viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0); break;
-  	case TopDown : viewer->setCameraPosition(0, 0, distance, 1, 0, 1); break;
-  	case Side : viewer->setCameraPosition(0, -distance, 0, 0, 0, 1); break;
-  	case FPS : viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
-  }
-  if(setAngle!=FPS)
-  	viewer->addCoordinateSystem (1.0);
-  
-
-  // Create point cloud processor
-  // TODO:: Create a point cloud processor
-
-  simpleHighway(viewer);
-  
-
-  while (!viewer->wasStopped ())
-  {
-    viewer->spinOnce ();
-  }
+  	while (!viewer->wasStopped ())
+  	{
+    	viewer->spinOnce ();
+  	}
   	
 }
