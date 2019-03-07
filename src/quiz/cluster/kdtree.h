7c2b70aeefa06276caf564feaf430adb1ec6c0ad
awbrown90 <aaronwbrown@msn.com>
2019-03-07 14:55:20 -0800

    Quiz: update cluster doc and revert kd tree source code to student version

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -25,38 +25,16 @@ struct KdTree
 	: root(NULL)
 	{}
 
-	void insertHelper(Node** node, uint depth, std::vector<float> point, int id)
-	{
-
-		// Tree is empty
-		if(*node==NULL)
-			*node = new Node(point,id);
-		else
-		{
-			// Calculate current dim
-			uint cd = depth % 2;
-	
-			if(point[cd] < ((*node)->point[cd]))
-				insertHelper(&((*node)->left), depth+1, point, id);
-			else
-				insertHelper(&((*node)->right), depth+1, point, id);
-		}
-
-	}
-
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-		insertHelper(&root,0,point,id);
+
 	}
 
 	// return a list of point ids in the tree that are within distance of pivot
 	std::vector<int> search(std::vector<float> pivot, float distanceTol)
 	{
-
-		// TODO: Fill in this function to return a list of indices of the points in the tree with in distanceTol of pivot
-
 		std::vector<int> ids;
 		return ids;
 	}
